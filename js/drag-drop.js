9686451a4ae0ac70d7eb6879584ac173feebd66a
wrqqq <wrqqqrrr@gmail.com>
2018-08-10 06:33:48 +0300

    Rewrite drag-drop

--- js/drag-drop.js
+++ js/drag-drop.js
@@ -1,7 +1,102 @@
 'use strict';
 
 (function() {
-    window.dragDrop = function(el) {
+    var sliding = 0,
+    startClientX = 0,
+    startPixelOffset = 0,
+    pixelOffset = 0,
+    currentSlide = 0,
+    slideCount = slider.querySelectorAll('.slider_item').length,
+    item = slider.querySelector('.slider_item');
+    window.dragDrop = function(slider) {
+        slider.ondragstart = function() {
+            return false;
+        };
+        slider.addEventListener('mousedown', startSlide);
+        slider.addEventListener('mouseup', slideEnd);
+        slider.addEventListener('mousemove', slide);
+
+        function startSlide(e) {
+            if (sliding == 0) {
+                sliding = 1;
+                startClientX = e.clientX;
+            }
+        }
+
+        function slide(e) {
+            e.preventDefault();
+            // Distance of slide.
+            var distance = e.clientX - startClientX;
+            // If sliding started first time and there was a distance.
+            if (sliding == 1 && distance != 0) {
+                sliding = 2; // Set status to 'actually moving'
+                startPixelOffset = pixelOffset; // Store current offset
+            }
+
+            if (sliding == 2) {
+                var touchPixelRatio = 1;
+                //console.log(currentSlide == slideCount/3 - 1);
+                //console.log(e.clientX < startClientX);
+
+                if (
+                    (currentSlide == 0 && e.clientX > startClientX) ||
+                    (currentSlide == slideCount/3 - 1 && e.clientX < startClientX)
+                )
+                    touchPixelRatio = 3;
+
+                pixelOffset = startPixelOffset + distance / touchPixelRatio;
+                /*slider.css("transform", "translateX(" + pixelOffset + "px")
+                    .removeClass();*/
+                slider.style.transform = 'translateX(' + pixelOffset + 'px' + ')';
+                slider.className = '';
+
+            }
+
+        }
+
+        function slideEnd(e) {
+            if (sliding == 2) {
+                // Reset sliding.
+                sliding = 0;
+                // Calculate which slide need to be in view.
+                currentSlide = pixelOffset < startPixelOffset ? currentSlide + 1 : currentSlide - 1;
+                // Make sure that unexisting slides weren't selected.
+                currentSlide = Math.min(Math.max(currentSlide, 0), slideCount - 1);
+                // Since in this example slide is full viewport width offset can be calculated according to it.
+                pixelOffset = currentSlide * -slider.offsetWidth;
+                if (currentSlide > slideCount/3 - 1) {
+                    pixelOffset = (slideCount/3 - 1) * -slider.offsetWidth;
+                    currentSlide = slideCount/3 - 1;
+                }
+                console.log(currentSlide);
+                // Remove style from DOM (look below)
+                //$("#temp").remove();
+
+                    if (document.querySelector('#temp')) {
+                        document.querySelector('#temp').remove();
+                    }
+                    //document.querySelector('#temp').remove();
+                // Add a translate rule dynamically and asign id to it
+
+                /*var style = '<style id="temp">#slider.animate{transform:translateX(' +
+                    pixelOffset + 'px)}</style>';*/
+                var css = "body #slider.animate{transform:translateX(" + pixelOffset + "px) }</style>",
+                    head = document.head || document.getElementsByTagName('head')[0],
+                    style = document.createElement('style');
+
+                style.id = 'temp';
+
+                    style.appendChild(document.createTextNode(css));
+
+                head.appendChild(style);
+                // Add animate class to slider and reset transform prop of this class.
+                slider.className = 'animate';
+                slider.style = '';
+
+            }
+        }
+        }
+        /*function(el) {
         window.slideMove = 0;
         el.onmousedown = function(e) {
             var startCoordinate = e.pageX;
@@ -9,23 +104,16 @@
             el.ondragstart = function() {
                 return false;
             };
-            function slideEl(bool) {
-                            if (bool) {
-                                /*window.transformValue -= slide;
-                                el.style.transform = 'translateX(' + window.transformValue + 'px' + ')';*/
-                            } else {
-                                /*window.transformValue += slide;
-                                el.style.transform = 'translateX(' + window.transformValue + 'px' + ')';*/
-                            }
-                            //el.style.transform = 'translateX(' + (slideMove) + 'px' + ')';
-
-            }
             document.onmousemove = function(e) {
+                window.transformValue = e.pageX - startCoordinate;
+                slider.style.transform = 'translateX(' + window.transformValue + 'px' + ')';
+
+
                 /*var start = 0;
                 start++;
                 console.log(start);
                 if (start == 1) {*/
-                    if (startCoordinate > e.pageX) {
+                    /*if (startCoordinate > e.pageX) {
                         window.moveSliderRight();
                     } else if (startCoordinate < e.pageX) {
                         window.moveSliderLeft();
@@ -42,7 +130,7 @@
                     slideEl(false);
                 }
 
-            };*/
+            };
 
             el.onmouseup = function() {
                 document.onmousemove = null;
@@ -54,5 +142,5 @@
             }
 
         }
-    }
+    }*/
 })();
--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
     var leftCtrl = document.querySelector('.slider-control--left');
     var rightCtrl = document.querySelector('.slider-control--right');
     var slider = document.querySelector('#slider');
-    var itemWidth = document.querySelector('.slider_item').offsetWidth;
+    var itemWidth = document.querySelector('.slider_item').offsetWidth + 20;
     window.transformValue = 0;
     var itemsCollection = document.querySelectorAll('.slider_item');
     var slice = function(elements, start, end) {
@@ -20,7 +20,7 @@
     function getSliderWidth(itemsCollection) {
         var count = 0;
         itemsCollection.forEach(function(item) {
-            count += item.offsetWidth;
+            count += item.offsetWidth - 20;
         });
         console.log(count);
         return count;
@@ -36,22 +36,25 @@
         }
 
         function moveSliderRight() {
-            if (-window.transformValue <= (window.sliderWidth - (itemWidth * 4))) {
-                console.log(-window.transformValue);
-                console.log(window.sliderWidth - (itemWidth * 3));
-                window.transformValue = window.transformValue - itemWidth;
-                console.log(window.transformValue);
-                slider.style.transform = 'translateX(' + window.transformValue + 'px' + ')';
+            if (screen.width > 1000) {
+                if (-window.transformValue <= (window.sliderWidth - (itemWidth * 3))) {
+                    console.log(-window.transformValue);
+                    console.log(window.sliderWidth - (itemWidth * 3));
+                    window.transformValue = window.transformValue - itemWidth;
+                    console.log(window.transformValue);
+                    slider.style.transform = 'translateX(' + window.transformValue + 'px' + ')';
+                }
+            } else {
+                if (-window.transformValue <= (window.sliderWidth - itemWidth)) {
+                    window.transformValue = window.transformValue - itemWidth;
+                    console.log(window.transformValue);
+                    slider.style.transform = 'translateX(' + window.transformValue + 'px' + ')';
+                }
             }
         }
 
         window.moveSliderLeft = moveSliderLeft;
         window.moveSliderRight = moveSliderRight;
-
-        /*function endlessItems() {
-
-        }*/
-
 
         window.dragDrop(slider);
         leftCtrl.addEventListener('click', moveSliderLeft);
